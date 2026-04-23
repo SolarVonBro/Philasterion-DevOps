6b9ce6db470e9ab961403ae4166819d105759adb
Holiienko Roman <solarvonbro@gmail.com>
2026-04-23 14:12:51 +0300

    Test run 1

--- resources/js/views/ProfileView.vue
+++ resources/js/views/ProfileView.vue
@@ -75,7 +75,7 @@
             <line x1="16" y1="13" x2="8" y2="13"/>
             <line x1="16" y1="17" x2="8" y2="17"/>
           </svg>
-          Дневник самочувствия
+          Дневник самочувствия 111
         </h2>
         <button v-if="!diary.showForm" class="btn-primary btn-sm" @click="openCreate">+ Новая запись</button>
       </div>
